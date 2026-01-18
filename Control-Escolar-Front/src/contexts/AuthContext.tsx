602a9c09a1b58537924dccc937c4dc42c0198259
IsmaelJrDev <ismaelbibiano.dev@gmail.com>
2026-01-18 13:45:19 -0600

    Integracion de perfil conectado al backend y a base de datos

--- Control-Escolar-Front/src/contexts/AuthContext.tsx
+++ Control-Escolar-Front/src/contexts/AuthContext.tsx
@@ -109,4 +109,4 @@ export const useAuth = () => {
   const context = useContext(AuthContext);
   if (!context) throw new Error('useAuth debe usarse dentro de un AuthProvider');
   return context;
-};
+};  
